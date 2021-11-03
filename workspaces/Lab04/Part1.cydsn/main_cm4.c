8514eaf04832dbca05a01456b86a0a71c56b5016
Lena Hickson Long <lhickson@ucsc.edu>
2021-11-03 15:17:56 -0700

    FINAL LAB 4

--- workspaces/Lab04/Part1.cydsn/main_cm4.c
+++ workspaces/Lab04/Part1.cydsn/main_cm4.c
@@ -14,6 +14,7 @@ void uart_printf(char *print_string);
 //Data ready flag
 bool data_ready = false;
 
+//Helper functions for INT masking
 void enable_mic_int() {
     PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() | CY_PDM_PCM_INTR_RX_NOT_EMPTY);
 }
@@ -46,7 +47,6 @@ int main(void)
     Cy_SysInt_Init(&PDM_PCM_1_INT_cfg, PDM_PCM_ISR);
     NVIC_EnableIRQ(PDM_PCM_1_INT_cfg.intrSrc);
     
-    
     //VDAC setup
     Cy_CTDAC_FastInit(CTDAC0, &Cy_CTDAC_Fast_VddaRef_UnbufferedOut);
     Cy_CTDAC_Enable(CTDAC0);
@@ -56,15 +56,16 @@ int main(void)
     uint16_t sample_buffer [BUFFER_SIZE];
     size_t sample_buffer_index = 0;
     
-    bool recording = false;
     //Data ready to send
     bool ready_to_play = false;
     
+    bool recording = false;
+    
+    //Start with interrupts disabled
     disable_mic_int();
     
     for(;;)
     {
-        //Attempt 3
         //Clear FIFO if we're not recording
         PDM_PCM_1_ClearFifo();
         
@@ -100,8 +101,6 @@ int main(void)
         }
         
         if(ready_to_play) {
-            
-            uart_printf("PLAYING\n\r");
          
             disable_mic_int();
             
@@ -112,124 +111,12 @@ int main(void)
                 
             }
             
-            uart_printf("DONE PLAYING\n\r");
-            
+            //Reset for recording next time
             sample_buffer_index = 0;
             ready_to_play = false;
             enable_mic_int();
             
         }
-        
-        
-        //Attempt 2
-        /*
-        //char recording_str[6];
-        //sprintf(recording_str, "%d\n\r", recording);
-        //uart_printf(recording_str);
-        
-        //Clear FIFO
-        PDM_PCM_1_ClearFifo();
-        
-        //Start recording
-        if(!ready_to_play && !recording && !Cy_GPIO_Read(switch_2_0_PORT, switch_2_0_NUM)) {
-            uart_printf("START recording");
-            recording = true;
-            enable_mic_int();
-        }
-        
-        //Stop recording
-        if(recording && 
-            (Cy_GPIO_Read(switch_2_0_PORT, switch_2_0_NUM) || sample_buffer_index >= BUFFER_SIZE)) {
-                
-            uart_printf("STOP recording");
-            recording = false;
-            ready_to_play = true;
-            disable_mic_int();
-        }  
-            
-        //Get mic data
-        if(data_ready) {
-            disable_mic_int();
-            
-            uint32_t rxData_raw = PDM_PCM_1_ReadFifo();
-            int16_t rxData_signed = (int16_t) rxData_raw;
-            uint16_t rxData_ready = (uint16_t) abs(rxData_signed);
-            sample_buffer[sample_buffer_index++] = rxData_ready;
-            
-            //char index_str[8];
-            //sprintf(index_str, "%d\n\r", sample_buffer_index);
-            //uart_printf(index_str);
-            
-            data_ready = false;
-            enable_mic_int();
-        }
-        
-        if(ready_to_play) {
-            
-            uart_printf("PLAYING\n\r");
-         
-            disable_mic_int();
-            
-            for(size_t i = 0; i < sample_buffer_index; i++) {
-             
-                Cy_CTDAC_SetValueBuffered(CTDAC0, sample_buffer[i]);
-                CyDelayUs(125);
-                
-            }
-            
-            uart_printf("DONE PLAYING\n\r");
-            
-            sample_buffer_index = 0;
-            ready_to_play = false;
-            enable_mic_int();
-            
-        }
-        */
-        
-        //Attempt 1
-        /*
-        //Read data from the Mic
-        if(data_ready) {
-            
-            //Buffer is not full
-            if(sample_buffer_index < BUFFER_SIZE) {
-                //FIFO is not empty
-                if(PDM_PCM_1_GetNumInFifo()) {
-                    
-                    //Save data
-                    uint32_t rxData_raw = PDM_PCM_1_ReadFifo();
-                    int16_t rxData_signed = (int16_t) rxData_raw;
-                    uint16_t rxData_ready = (uint16_t) abs(rxData_signed);
-                    sample_buffer[sample_buffer_index++] = rxData_ready;
-                            
-                } 
-            }
-            
-            data_ready = false;
-            //Enable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() | CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            ready_to_play = true;
-        }
-        
-        if(ready_to_play) {
-         
-            //Disable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() & ~CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            
-            for(size_t i = 0; i < BUFFER_SIZE; i++) {
-             
-                Cy_CTDAC_SetValueBuffered(CTDAC0, sample_buffer[i]);
-                CyDelay(125);
-                
-            }
-            
-            ready_to_play = false;
-            //Enable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() | CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            
-        }
-        */
-        
         
     }
 }
--- workspaces/Lab04/Part2.cydsn/main_cm4.c
+++ workspaces/Lab04/Part2.cydsn/main_cm4.c
@@ -38,7 +38,9 @@ int main(void)
     
     //SAR->CHAN_RESULT[0] is the index of the ADC registry
     Cy_DMA_Descriptor_Init(&DMA_1_Descriptor_1,&DMA_1_Descriptor_1_config);
+    //src -> PDM_PCM RX FIFO pointer
     Cy_DMA_Descriptor_SetSrcAddress(&DMA_1_Descriptor_1,(uint32_t *) &(PDM_PCM_1_HW->RX_FIFO_RD));
+    //dst -> sample buffer base pointer
     Cy_DMA_Descriptor_SetDstAddress(&DMA_1_Descriptor_1,sample_buffer);
     
     bool recording = false;
@@ -47,7 +49,6 @@ int main(void)
     
     for(;;)
     {
-        //Attempt 3
         
         //Start recording
         if(!ready_to_play && !recording && !Cy_GPIO_Read(switch_2_0_PORT, switch_2_0_NUM)) {
@@ -55,7 +56,6 @@ int main(void)
             Cy_DMA_Enable(DMA_1_HW);
             Cy_DMA_Channel_Enable(DMA_1_HW, DMA_1_DW_CHANNEL);
             recording = true;
-            uart_printf("recording...\n\r");
         }
         
         //Stop recording
@@ -64,25 +64,17 @@ int main(void)
             Cy_DMA_Channel_Disable(DMA_1_HW, DMA_1_DW_CHANNEL);
             recording = false;
             ready_to_play = true;
-            uart_printf("done recording\n\r");
         }
         
         if(ready_to_play) {
 
+            //Get the index that DMA made it to
             size_t count = DMA_1_HW->CH_STRUCT[DMA_1_DW_CHANNEL].CH_IDX;
             //Reset DMA
             Cy_DMA_Channel_DeInit(DMA_1_HW, DMA_1_DW_CHANNEL);
             
             //If DMA completed, set count to the end of the buffer
             count = !count ? BUFFER_SIZE : count;
-            
-            char count_str[8];
-            sprintf(count_str, "%d\n\r", count);
-            uart_printf(count_str);
-
-            
-            uart_printf("PLAYING\n\r");
-         
             
             for(size_t i = 0; i < count; i++) {
                 
@@ -96,122 +88,9 @@ int main(void)
                 
             }
             
-            uart_printf("DONE PLAYING\n\r");
-            
             ready_to_play = false;
             
         }
-        
-        
-        //Attempt 2
-        /*
-        //char recording_str[6];
-        //sprintf(recording_str, "%d\n\r", recording);
-        //uart_printf(recording_str);
-        
-        //Clear FIFO
-        PDM_PCM_1_ClearFifo();
-        
-        //Start recording
-        if(!ready_to_play && !recording && !Cy_GPIO_Read(switch_2_0_PORT, switch_2_0_NUM)) {
-            uart_printf("START recording");
-            recording = true;
-            enable_mic_int();
-        }
-        
-        //Stop recording
-        if(recording && 
-            (Cy_GPIO_Read(switch_2_0_PORT, switch_2_0_NUM) || sample_buffer_index >= BUFFER_SIZE)) {
-                
-            uart_printf("STOP recording");
-            recording = false;
-            ready_to_play = true;
-            disable_mic_int();
-        }  
-            
-        //Get mic data
-        if(data_ready) {
-            disable_mic_int();
-            
-            uint32_t rxData_raw = PDM_PCM_1_ReadFifo();
-            int16_t rxData_signed = (int16_t) rxData_raw;
-            uint16_t rxData_ready = (uint16_t) abs(rxData_signed);
-            sample_buffer[sample_buffer_index++] = rxData_ready;
-            
-            //char index_str[8];
-            //sprintf(index_str, "%d\n\r", sample_buffer_index);
-            //uart_printf(index_str);
-            
-            data_ready = false;
-            enable_mic_int();
-        }
-        
-        if(ready_to_play) {
-            
-            uart_printf("PLAYING\n\r");
-         
-            disable_mic_int();
-            
-            for(size_t i = 0; i < sample_buffer_index; i++) {
-             
-                Cy_CTDAC_SetValueBuffered(CTDAC0, sample_buffer[i]);
-                CyDelayUs(125);
-                
-            }
-            
-            uart_printf("DONE PLAYING\n\r");
-            
-            sample_buffer_index = 0;
-            ready_to_play = false;
-            enable_mic_int();
-            
-        }
-        */
-        
-        //Attempt 1
-        /*
-        //Read data from the Mic
-        if(data_ready) {
-            
-            //Buffer is not full
-            if(sample_buffer_index < BUFFER_SIZE) {
-                //FIFO is not empty
-                if(PDM_PCM_1_GetNumInFifo()) {
-                    
-                    //Save data
-                    uint32_t rxData_raw = PDM_PCM_1_ReadFifo();
-                    int16_t rxData_signed = (int16_t) rxData_raw;
-                    uint16_t rxData_ready = (uint16_t) abs(rxData_signed);
-                    sample_buffer[sample_buffer_index++] = rxData_ready;
-                            
-                } 
-            }
-            
-            data_ready = false;
-            //Enable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() | CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            ready_to_play = true;
-        }
-        
-        if(ready_to_play) {
-         
-            //Disable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() & ~CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            
-            for(size_t i = 0; i < BUFFER_SIZE; i++) {
-             
-                Cy_CTDAC_SetValueBuffered(CTDAC0, sample_buffer[i]);
-                CyDelay(125);
-                
-            }
-            
-            ready_to_play = false;
-            //Enable RX FIFO NOT EMPTY interrupt
-            PDM_PCM_1_SetInterruptMask(PDM_PCM_1_GetInterruptMask() | CY_PDM_PCM_INTR_RX_NOT_EMPTY);
-            
-        }
-        */
-        
         
     }
 }
