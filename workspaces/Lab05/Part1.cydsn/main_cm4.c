3764e0cccfd1a0c6b3aaba6899255d735ac023ac
Lena Skylar Hickson Long <lhickson@ucsc.edu>
2021-11-12 19:45:00 -0800

    kinda working error task, pointers are weird

--- workspaces/Lab05/Part1.cydsn/main_cm4.c
+++ workspaces/Lab05/Part1.cydsn/main_cm4.c
@@ -13,6 +13,7 @@
 #define BIT_TIME (100000/BAUD_RATE)  /* Bit time in ticks */ 
 
 QueueHandle_t print_queue;
+QueueHandle_t error_queue;
 
 void uart_printf(void){ 
     char print_string[100]; // string to print 
@@ -26,6 +27,29 @@ void uart_printf(void){
         Cy_SCB_UART_PutString(UART_PRINTF_HW, print_string);
     } 
 } 
+
+void soft_uart_error(void) {
+    uint8_t* error_byte;
+    
+    for (;;){ 
+        xQueueReceive(error_queue, error_byte, portMAX_DELAY);
+        continue;
+        if(*error_byte == 1) {
+            //Set LED to full brightness
+            // GPIO ON CODE HERE
+            //Delay for 5 seconds at 100khz
+            vTaskDelay(500000);
+        } else if(*error_byte == 2) {
+            //Set LED to half brightness
+            int ending_time = xTaskGetTickCount() + 500000;
+            int light_switch = 1;
+            while(xTaskGetTickCount() < ending_time) {
+                // GPIO SET TO light_switch
+                light_switch = !light_switch;
+            }
+        }
+    } 
+}
 
 void soft_uart_reciever(void) {
     char recieved_data[100];
@@ -57,14 +81,20 @@ void soft_uart_reciever(void) {
             bit = bit << i; //Shift bit to its place, assuming LSB transmitted first
             byte = byte | bit; //Save bit to byte
         }
+        
         sprintf(recieved_data, "%x\r\n", byte); //put byte in output string
+        
+        //Error checking
+        //uint8_t *err;
+        //*err = 0;
         
         vTaskDelay(BIT_TIME);
         //We are on the parity bit
         uint8_t bit = Status_Reg_1_Read(); //exactly one bit
         parity += bit;
         if(parity % 2 == 0) { //Check for odd parity
-            xQueueSend(print_queue, "bad parity\r\n", 0);
+            //*err = 1;
+            //xQueueSend(soft_uart_error, err, 0); //Print the string
             continue;
         }
         
@@ -72,12 +102,11 @@ void soft_uart_reciever(void) {
         //We are on the stop bit
         //Verify stop bit is high
         if(!Status_Reg_1_Read()) {
-            xQueueSend(print_queue, "bad stop bit\r\n", 0);
+            //*err = 2;
+            //xQueueSend(soft_uart_error, err, 0); //Print the string
             continue;
         }
-
         xQueueSend(print_queue, recieved_data, 0); //Print the string
-
         
         while(!Status_Reg_1_Read()) { //wait until we go high
             vTaskDelay(1);
@@ -90,9 +119,10 @@ int main(void)
     __enable_irq(); 
     
     print_queue = xQueueCreate(4, 100);
-    xQueueSend(print_queue, "Goodbye World\r\n", 0);   
+    //error_queue = xQueueCreate(4, 1);
     
     xTaskCreate(soft_uart_reciever, "SOFT_UART_RECIEVE", 400, NULL, 2, 0); 
+    //xTaskCreate(soft_uart_error, "SOFT_UART_ERR", 400, NULL, 1, 0); 
     xTaskCreate(uart_printf, "UART_PRINTF", 400, NULL, 1, 0); 
     
     vTaskStartScheduler(); /* Start FreeRTOS scheduler */ 
